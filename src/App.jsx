6f04a69480d7b397349d60c718e5d3895f8f7479
Sathiya@1998 <sathiyavenkatachalam258@gmail.com>
2024-05-24 07:42:19 +0530

    array list and keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 
-const Hello = () => {
-  return <h1>Hello , World</h1>
-}
-
-// Component App
 const App = () => {
+
+  const friends =[
+    {id: 1 , name: 'John' , age: 22},
+    {id: 2 , name: 'Jane' , age: 23},
+    {id: 3 , name: 'Siva' , age: 24},
+  ]
   return (
     <div>
-       <Hello />
-       <Hello />
-       <Hello />
-    </div>
-  ) 
+      <h1>Friends</h1>
+      <ul>
+        {
+          friends.map((friend) =>
+          <li key ={friend.id}>{freind.name} {friend.age}</li>
+        )
+        }
+      </ul>
+      
+      </div>
+  )
 }
-export default App;
+
+export default App
