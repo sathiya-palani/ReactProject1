5b9e83fd313a3d2bc11ea0e40114221683f281ba
Sathiya@1998 <sathiyavenkatachalam258@gmail.com>
2024-05-24 08:06:10 +0530

    conditional rendering using  --- logical and

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@
 
 const App = () => {
 
-  const isLoggedIn = false;
+  const isLoggedIn = true;
 
   return (
     <div>
-      {
-        isLoggedIn ? <h1>Welcome back!</h1> : <h1>Please log In</h1>
-      }
+      { isLoggedIn && <h1>Welcome back!</h1>}
+      { !isLoggedIn && <h1>Please log In</h1>}
+      
     </div>
   )
     
