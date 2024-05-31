6b84a3a0e70877695ab578d334ed96f63202b96e
Sathiya@1998 <sathiyavenkatachalam258@gmail.com>
2024-05-31 11:21:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { createContext , useContext , useState } from "react";
 
+ //1.Create a context to store the name 
+const NameContext = createContext();
+
+const D =() => {
+
+  //3.Use the context in the component
+  const{name} = useContext(NameContext);
+
+  return <h1>Hello ,{name} !</h1>
+}
+
+const C = () => {
+  return <D />
+}
+
+const B = () => {
+  return <C />
+}
+
+//2.Wrap the component tree with the context provider
 const App = () => {
-  return <h1>Hello, world</h1>
+    const [name , setName] =useState("John");
+
+    return (
+      <NameContext.Provider value ={{name}}>
+        <B />
+      </NameContext.Provider>
+    )
 }
 export default App;
