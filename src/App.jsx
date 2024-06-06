7ed329b545e9fec52753dbefaea648c15f1945d0
Sathiya@1998 <sathiyavenkatachalam258@gmail.com>
2024-06-06 17:52:11 +0530

    CRUD operations done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Todos from "./pages/Todos";
+import { loader as todosLoader } from "./pages/Todos";
 
 // create a router object
-const router = createBrowserRouter ([
-{
-  path: "/",
-  element:<h1>Hello world</h1>
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Todos />,
+    // link the loader function to the route
+    loader: todosLoader
+  }
 ]);
-   
-const App =() => {
-  return<RouterProvider router={router} />;
+
+
+const App = () => {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
